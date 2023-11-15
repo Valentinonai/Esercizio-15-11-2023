a7c6eb58bfceb4eb71dd8c28da6cf2daee0665a1
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-11-15 13:18:13 +0100

    Initial commit

---
+++ src/main/java/Esercizio15112023/Esercizio15112023/Esercizio15112023Application.java
@@ -0,0 +1,13 @@
+package Esercizio15112023.Esercizio15112023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercizio15112023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercizio15112023Application.class, args);
+	}
+
+}
---
+++ src/test/java/Esercizio15112023/Esercizio15112023/Esercizio15112023ApplicationTests.java
@@ -0,0 +1,13 @@
+package Esercizio15112023.Esercizio15112023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizio15112023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
